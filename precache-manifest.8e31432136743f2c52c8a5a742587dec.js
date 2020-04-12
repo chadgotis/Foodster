df8abfdd09ceab5211c0837bcfdafe9feb7c7fab
Chad Gotis <Chadgotis97@gmail.com>
2020-04-13 00:12:25 +0800

    Updates

---
+++ precache-manifest.8e31432136743f2c52c8a5a742587dec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f978ed7548f14a31925b5a0a56ce81",
+    "url": "/Foodster/index.html"
+  },
+  {
+    "revision": "1e37fe096bdfaf922003",
+    "url": "/Foodster/static/css/main.5fa2ec03.chunk.css"
+  },
+  {
+    "revision": "6583dd56ded178a1b07a",
+    "url": "/Foodster/static/js/2.dcfb351f.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/Foodster/static/js/2.dcfb351f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e37fe096bdfaf922003",
+    "url": "/Foodster/static/js/main.50ff0155.chunk.js"
+  },
+  {
+    "revision": "171840c27bcf5df2a55b",
+    "url": "/Foodster/static/js/runtime-main.f6deeea7.js"
+  },
+  {
+    "revision": "9e420f30f4adffb2be20d6a9cd2e8ee5",
+    "url": "/Foodster/static/media/hero.9e420f30.png"
+  }
+]);
